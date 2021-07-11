d2f625648f1ebf2cb46944052c3dc58413d35d21
Captain9965 <lennyweda@gmail.com>
2021-07-11 21:16:48 +0300

    Error page styling

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,8 +1,11 @@
-
+import { Link } from "react-router-dom";
 const Error=()=> {
     return (
         <>
+        <section className="error">
             <h1>Error 404 Page not found</h1>
+            <Link className="link" to="/">Homepage</Link>
+        </section>
         </>
     );
 }
